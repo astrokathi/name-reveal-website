d9c2e2737e7bf31a36c44e504da6bcf6d066aefd
Sai Sravan Kathi <saisravankathi@gmail.com>
2026-05-25 02:53:53 +0400

    New update

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68bf33e4c417c490"; }
+/******/ 		__webpack_require__.h = function() { return "6c8c222ec4a4348c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- docs/_next/static/webpack/webpack.d2db71330fa1e0ee.hot-update.js
+++ docs/_next/static/webpack/webpack.d2db71330fa1e0ee.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "68bf33e4c417c490"; }
+/******/ 	__webpack_require__.h = function() { return "6c8c222ec4a4348c"; }
 /******/ }();
 /******/ 
 /******/ }
